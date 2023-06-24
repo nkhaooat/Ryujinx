e7645a22c949f5caa7e335a3d1f88bb46bfc9682
gdk <gab.dark.100@gmail.com>
2023-06-24 17:06:00 -0300

    Skeleton code generation for vertex as compute

---
+++ src/Ryujinx.Graphics.Shader/Translation/ResourceReservations.cs
@@ -0,0 +1,134 @@
+using Ryujinx.Graphics.Shader.IntermediateRepresentation;
+using Ryujinx.Graphics.Shader.StructuredIr;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Ryujinx.Graphics.Shader.Translation
+{
+    class ResourceReservations
+    {
+        public const int TfeBuffersCount = 4;
+
+        public const int MaxVertexBufferTextures = 32;
+
+        public int ReservedConstantBuffers { get; }
+        public int ReservedStorageBuffers { get; }
+        public int ReservedTextures { get; }
+        public int ReservedImages { get; }
+        public int OutputSizePerInvocation { get; }
+        public int OutputSizeInBytesPerInvocation => OutputSizePerInvocation * sizeof(uint);
+
+        private readonly int _tfeInfoSbBinding;
+        private readonly int _tfeBufferSbBaseBinding;
+        private readonly int _vertexInfoCbBinding;
+        private readonly int _vertexOutputSbBinding;
+        private readonly int _indexBufferTextureBinding;
+        private readonly int _vertexBufferTextureBaseBinding;
+
+        private readonly Dictionary<IoDefinition, int> _outputOffsets;
+
+        public IReadOnlyDictionary<IoDefinition, int> OutputOffsets => _outputOffsets;
+
+        public ResourceReservations(bool isTransformFeedbackEmulated, bool vertexAsCompute, int vacOutputMap)
+        {
+            // All stages reserves the first constant buffer binding for the support buffer.
+            ReservedConstantBuffers = 1;
+            ReservedStorageBuffers = 0;
+            ReservedTextures = 0;
+            ReservedImages = 0;
+
+            if (isTransformFeedbackEmulated)
+            {
+                // Transform feedback emulation currently always uses 5 storage buffers.
+                _tfeInfoSbBinding = ReservedStorageBuffers;
+                _tfeBufferSbBaseBinding = ReservedStorageBuffers + 1;
+                ReservedStorageBuffers = 1 + TfeBuffersCount;
+            }
+
+            if (vertexAsCompute)
+            {
+                // One constant buffer reserved for vertex related state.
+                _vertexInfoCbBinding = ReservedConstantBuffers++;
+
+                // One storage buffer for the output vertex data.
+                _vertexOutputSbBinding = ReservedStorageBuffers++;
+
+                // Enough textures reserved for all vertex attributes, plus the index buffer.
+                _indexBufferTextureBinding = ReservedTextures;
+                _vertexBufferTextureBaseBinding = ReservedTextures + 1;
+                ReservedTextures += 1 + MaxVertexBufferTextures;
+            }
+
+            _outputOffsets = new Dictionary<IoDefinition, int>();
+
+            if (vertexAsCompute)
+            {
+                int offset = 0;
+
+                for (int c = 0; c < 4; c++)
+                {
+                    _outputOffsets.Add(new IoDefinition(StorageKind.Output, IoVariable.Position, 0, c), offset++);
+                }
+
+                while (vacOutputMap != 0)
+                {
+                    int location = BitOperations.TrailingZeroCount(vacOutputMap);
+
+                    for (int c = 0; c < 4; c++)
+                    {
+                        _outputOffsets.Add(new IoDefinition(StorageKind.Output, IoVariable.UserDefined, location, c), offset++);
+                    }
+
+                    vacOutputMap &= ~(1 << location);
+                }
+
+                OutputSizePerInvocation = offset;
+            }
+        }
+
+        public int GetTfeInfoStorageBufferBinding()
+        {
+            return _tfeInfoSbBinding;
+        }
+
+        public int GetTfeBufferStorageBufferBinding(int bufferIndex)
+        {
+            return _tfeBufferSbBaseBinding + bufferIndex;
+        }
+
+        public int GetVertexInfoConstantBufferBinding()
+        {
+            return _vertexInfoCbBinding;
+        }
+
+        public int GetVertexOutputStorageBufferBinding()
+        {
+            return _vertexOutputSbBinding;
+        }
+
+        public int GetIndexBufferTextureBinding()
+        {
+            return _indexBufferTextureBinding;
+        }
+
+        public int GetVertexBufferTextureBinding(int vaLocation)
+        {
+            return _vertexBufferTextureBaseBinding + vaLocation;
+        }
+
+        public bool TryGetOutputOffset(int location, int component, out int offset)
+        {
+            return _outputOffsets.TryGetValue(new IoDefinition(StorageKind.Output, IoVariable.UserDefined, location, component), out offset);
+        }
+
+        public bool TryGetOutputOffset(IoVariable ioVariable, int component, out int offset)
+        {
+            return _outputOffsets.TryGetValue(new IoDefinition(StorageKind.Output, ioVariable, 0, component), out offset);
+        }
+
+        public bool TryGetOutputOffset(IoVariable ioVariable, out int offset)
+        {
+            return _outputOffsets.TryGetValue(new IoDefinition(StorageKind.Output, ioVariable, 0, 0), out offset);
+        }
+    }
+}
